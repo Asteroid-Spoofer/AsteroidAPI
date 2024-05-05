2c2f0b2a495a92e0a02897acd48c4663f2d7b35c
SerbanHiro <51057957+SerbanHiro@users.noreply.github.com>
2024-05-05 04:28:45 +0300

    todo for death fake player

--- src/main/java/me/serbob/asteroidapi/Events/Impl/FakePlayerDeathEvent.java
+++ src/main/java/me/serbob/asteroidapi/Events/Impl/FakePlayerDeathEvent.java
@@ -3,11 +3,14 @@ package me.serbob.asteroidapi.Events.Impl;
 import me.serbob.asteroidapi.Events.FakePlayerEvent;
 import me.serbob.asteroidapi.Registries.FakePlayerEntity;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class FakePlayerDeathEvent extends FakePlayerEvent {
     private static final HandlerList handlerList = new HandlerList();
+    // TODO MORE
     public FakePlayerDeathEvent(FakePlayerEntity fakePlayer) {
         super(fakePlayer);
+
     }
     @Override
     public HandlerList getHandlers() {
